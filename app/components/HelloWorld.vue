1cd241d2d3df7503e5ec769b70a17db52a6290a2
Irfan <irfannaqieb@gmail.com>
2025-08-10 16:15:45 +0800

    tailwind and shadcn installation

--- app/components/HelloWorld.vue
+++ app/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
 	<div>
-		<h1 class="text-3xl font-bold underline">Hello World From Component</h1>
+		<h1 class="text-3xl font-bold underline border-2 border-red-500">
+			Hello World From Component
+		</h1>
 	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
-	modules: ["@nuxtjs/tailwindcss"],
+	css: ["~/assets/css/main.css"],
+	modules: ["shadcn-nuxt"],
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: "",
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: "./components/ui",
+	},
 });
