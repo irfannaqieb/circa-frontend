14f1b0937431a65b7637599d55146d985573d637
Irfan <irfannaqieb@gmail.com>
2025-08-10 16:25:00 +0800

    configured google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	css: ["~/assets/css/main.css"],
-	modules: ["shadcn-nuxt"],
+	modules: ["shadcn-nuxt", "@nuxtjs/google-fonts"],
 	vite: {
 		plugins: [tailwindcss()],
 	},
@@ -18,5 +18,14 @@ export default defineNuxtConfig({
 		 * @default "./components/ui"
 		 */
 		componentDir: "./components/ui",
+	},
+	googleFonts: {
+		families: {
+			Urbanist: [400, 500, 600, 700, 800], // choose weights you need
+		},
+		display: "swap", // prevents layout shifts
+		preconnect: true, // faster load
+		download: true, // self-host font files
+		inject: true, // auto inject <link> or @font-face
 	},
 });
