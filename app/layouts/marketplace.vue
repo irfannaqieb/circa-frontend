8c0a16dd9a57c6241204d20658638293539dfa1f
Irfan <irfannaqieb@gmail.com>
2025-08-16 13:08:00 +0800

    initial phase of marketplace layout

---
+++ app/layouts/marketplace.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="flex">
+		<MarketplaceSidebar />
+		<main class="flex-1 p-4">
+			<slot />
+		</main>
+	</div>
+</template>
+<script setup lang="ts">
+import MarketplaceSidebar from "~/components/Marketplace/Sidebar.vue";
+</script>
