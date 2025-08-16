334da4a0918da9c95d12cbe428da892d2aa1acae
Irfan <irfannaqieb@gmail.com>
2025-08-16 13:08:50 +0800

    refactor marketplace layout: remove old marketplace.vue and enhance index.vue with new header, filter, and sort buttons

--- app/pages/marketplace/index.vue
+++ app/pages/marketplace/index.vue
@@ -1,7 +1,21 @@
 <template>
 	<div>
-		<h1>Marketplace Home</h1>
-		<div class="p-4 flex space-x-4">
+		<div class="flex justify-between items-center mb-4">
+			<h1 class="text-2xl font-bold">For You</h1>
+			<div class="flex space-x-2">
+				<Button variant="outline">
+					<ListFilter class="mr-2 h-4 w-4" />
+					Filter
+				</Button>
+				<Button variant="outline">
+					<ArrowUpDown class="mr-2 h-4 w-4" />
+					Sort
+				</Button>
+			</div>
+		</div>
+		<div
+			class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
+		>
 			<MarketplaceItemCard status="Available" />
 			<MarketplaceItemCard status="Reserved" />
 			<MarketplaceItemCard status="Sold" />
@@ -12,6 +26,13 @@
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ListFilter, ArrowUpDown } from "lucide-vue-next";
+import { Button } from "~/components/ui/button";
+
+definePageMeta({
+	layout: "marketplace",
+});
+</script>
 
 <style scoped></style>
